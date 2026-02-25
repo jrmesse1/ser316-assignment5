32c4af8b8159eccbffd010500e51ecb6d4ada899
Jordan Messer <jrmesse1@asu.edu>
2026-02-25 14:26:42 -0500

    Add basic Staff class

---
+++ src/main/java/Staff.java
@@ -0,0 +1,28 @@
+public class Staff {
+    private String name;
+    private Role role;
+
+    public Staff(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public enum Role {
+        volunteer, veterinarian, technician, receptionist
+    }
+}
