f41659c13f9937f64fef6e1334be38e87f3f30ca
Jordan Messer <jrmesse1@asu.edu>
2026-02-25 19:43:47 -0500

    Add basic Animal class

---
+++ src/main/java/Animal.java
@@ -0,0 +1,15 @@
+public class Animal {
+    private String id;
+    private String species;
+    private String name;
+    private int age;
+    private int weight;
+
+    public Animal(String id, String species, String name, int age, int weight) {
+        this.id = id;
+        this.species = species;
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+}
