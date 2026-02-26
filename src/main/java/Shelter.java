dece4ca6da38cd598f2a2721e48ceceb0bb49e10
Jordan Messer <jrmesse1@asu.edu>
2026-02-25 19:47:42 -0500

    Add a basic Shelter class

---
+++ src/main/java/Shelter.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Shelter {
+    private ArrayList<Animal> animals;
+    private ArrayList<Staff> staff;
+
+    public Shelter() {
+        animals = new ArrayList<>();
+        staff = new ArrayList<>();
+    }
+}
