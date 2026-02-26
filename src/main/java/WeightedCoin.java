9e4c8129198cdb1fd249e0a288fde988ab9d6f4c
Jordan Messer <jrmesse1@asu.edu>
2026-02-25 21:17:45 -0500

    Add Coin class for random events

---
+++ src/main/java/WeightedCoin.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class WeightedCoin {
+    private int winPercentage;
+
+    /**
+     * Coin flip that will win a certain percentage of the time.
+     * @param winPercentage
+     */
+    public WeightedCoin(int winPercentage) {
+        winPercentage = winPercentage;
+    }
+
+    public boolean flip() {
+        Random random = new Random();
+        return winPercentage >= random.nextInt(101) ;
+    }
+}
