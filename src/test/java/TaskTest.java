8a01cedca3e98c9c0afa9a46e1cd17e749fe8d19
Jordan Messer <jrmesse1@asu.edu>
2026-02-28 15:20:49 -0500

    Add tests for Task class

---
+++ src/test/java/TaskTest.java
@@ -0,0 +1,186 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+    @Test
+    void getAndSetAssignee() {
+        Animal animal = new Animal(0, "Tester");
+        Staff staff = new Staff(0, "Tested", StaffRole.VETERINARIAN);
+        Task task = new TaskDailyFeeding(animal);
+        task.setAssignee(staff);
+        assertEquals(staff, task.getAssignee());
+        task.setAssignee(null);
+        assertNull(task.getAssignee());
+    }
+
+    @Test
+    void decrementTimeRemaining() {
+        Animal animal = new Animal(0, "Tester");
+        Task task = new TaskDailyFeeding(animal);
+        for (int i = 0; i < task.getDuration(); i++) {
+            // task takes number of decrements equal to duration to complete
+            assertFalse(task.isComplete());
+            task.decrementTimeRemaining();
+        }
+        assertTrue(task.isComplete());
+    }
+
+    @Test
+    void isDiscardedTaskExercise() {
+        Animal animal = new Animal(0, "Tester");
+        Task task = new TaskDailyExercise(animal);
+        assertFalse(task.isDiscarded());
+        animal.setStatus(AnimalStatus.ADOPTED);
+        assertFalse(task.isStillNeeded(animal));
+        task.discardIfNotNeeded();
+        assertTrue(task.isDiscarded());
+    }
+
+    @Test
+    void isDiscardedTaskFeeding() {
+        Animal animal = new Animal(0, "Tester");
+        Task task = new TaskDailyFeeding(animal);
+        assertFalse(task.isDiscarded());
+        animal.setStatus(AnimalStatus.ADOPTED);
+        assertFalse(task.isStillNeeded(animal));
+        task.discardIfNotNeeded();
+        assertTrue(task.isDiscarded());
+    }
+
+    @Test
+    void isDiscardedTaskEnclosureCleaning() {
+        Animal animal = new Animal(0, "Tester");
+        Task task = new TaskEnclosureCleaning(animal);
+        assertFalse(task.isDiscarded());
+        animal.setStatus(AnimalStatus.ADOPTED);
+        assertFalse(task.isStillNeeded(animal));
+        task.discardIfNotNeeded();
+        assertTrue(task.isDiscarded());
+    }
+
+    @Test
+    void isDiscardedTaskIntakeExam() {
+        Animal animal = new Animal(0, "Tester");
+        Task task = new TaskIntakeExam(animal);
+        assertFalse(task.isDiscarded());
+        animal.setStatus(AnimalStatus.NEEDS_VACCINATION);
+        assertFalse(task.isStillNeeded(animal));
+        task.discardIfNotNeeded();
+        assertTrue(task.isDiscarded());
+    }
+
+
+    @Test
+    void isDiscardedTaskVaccination() {
+        Animal animal = new Animal(0, "Tester");
+        Task task = new TaskVaccination(animal);
+        assertFalse(task.isDiscarded());
+        animal.setStatus(AnimalStatus.AVAILABLE);
+        assertFalse(task.isStillNeeded(animal));
+        task.discardIfNotNeeded();
+        assertTrue(task.isDiscarded());
+    }
+
+    @Test
+    void getTimeWorkedHours() {
+        Animal animal = new Animal(0, "Tester");
+        Task task = new TaskDailyFeeding(animal);
+        assertEquals(0.0, task.getTimeWorkedHours());
+        task.decrementTimeRemaining();
+        assertEquals(1.0 / 60.0, task.getTimeWorkedHours());
+    }
+
+    @Test
+    void getName() {
+        Animal animal = new Animal(0, "Tester");
+        Task task = new TaskDailyExercise(animal);
+        assertEquals("daily exercise", task.getName());
+        task = new TaskDailyFeeding(animal);
+        assertEquals("daily feeding", task.getName());
+        task = new TaskEnclosureCleaning(animal);
+        assertEquals("enclosure cleaning", task.getName());
+        task = new TaskIntakeExam(animal);
+        assertEquals("intake exam", task.getName());
+        task = new TaskVaccination(animal);
+        assertEquals("vaccination", task.getName());
+    }
+
+    @Test
+    void getDuration() {
+        Animal animal = new Animal(0, "Tester");
+        Task task = new TaskDailyExercise(animal);
+        assertEquals(30, task.getDuration());
+        task = new TaskDailyFeeding(animal);
+        assertEquals(10, task.getDuration());
+        task = new TaskEnclosureCleaning(animal);
+        assertEquals(100, task.getDuration());
+        task = new TaskIntakeExam(animal);
+        assertEquals(120, task.getDuration());
+        task = new TaskVaccination(animal);
+        assertEquals(30, task.getDuration());
+    }
+
+    @Test
+    void completeTaskExercise() {
+        Animal animal = new Animal(0, "Tester");
+        Task task = new TaskDailyExercise(animal);
+        while (!task.isComplete()) task.decrementTimeRemaining();
+        assertTrue(task.isComplete());
+    }
+
+    @Test
+    void completeTaskFeeding() {
+        Animal animal = new Animal(0, "Tester");
+        Task task = new TaskDailyFeeding(animal);
+        while (!task.isComplete()) task.decrementTimeRemaining();
+        assertTrue(task.isComplete());
+    }
+
+    @Test
+    void completeTaskEnclosureCleaning() {
+        Animal animal = new Animal(0, "Tester");
+        Task task = new TaskEnclosureCleaning(animal);
+        while (!task.isComplete()) task.decrementTimeRemaining();
+        assertTrue(task.isComplete());
+    }
+
+    @Test
+    void completeTaskIntakeExam() {
+        Animal animal = new Animal(0, "Tester");
+        Task task = new TaskIntakeExam(animal);
+        while (!task.isComplete()) task.decrementTimeRemaining();
+        assertTrue(task.isComplete());
+
+        // animal status should change in onComplete fn
+        assertEquals(AnimalStatus.NEEDS_VACCINATION, animal.getStatus());
+    }
+
+
+    @Test
+    void completeTaskVaccination() {
+        Animal animal = new Animal(0, "Tester");
+        animal.setStatus(AnimalStatus.NEEDS_VACCINATION);
+        Task task = new TaskVaccination(animal);
+        while (!task.isComplete()) task.decrementTimeRemaining();
+        assertTrue(task.isComplete());
+
+        // animal status should change in onComplete fn
+        assertEquals(AnimalStatus.AVAILABLE, animal.getStatus());
+    }
+
+    @Test
+    void testToString() {
+        Animal animal = new Animal(0, "Tester");
+        Task task = new TaskDailyExercise(animal);
+        assertEquals("daily exercise for Tester", task.toString());
+        task = new TaskDailyFeeding(animal);
+        assertEquals("daily feeding for Tester", task.toString());
+        task = new TaskEnclosureCleaning(animal);
+        assertEquals("enclosure cleaning for Tester", task.toString());
+        task = new TaskIntakeExam(animal);
+        assertEquals("intake exam for Tester", task.toString());
+        task = new TaskVaccination(animal);
+        assertEquals("vaccination for Tester", task.toString());
+    }
+}
